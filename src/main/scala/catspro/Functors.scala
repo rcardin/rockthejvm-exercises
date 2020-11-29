73d4e272995d1b7d8293552efda7963ef5533718
rcardin <riccardo.cardin@gmail.com>
2020-11-29 22:02:28 +0100

    Finished exercises on Functors

--- src/main/scala/catspro/Functors.scala
+++ src/main/scala/catspro/Functors.scala
@@ -1,6 +1,6 @@
 package catspro
 
-class Functors {
+object Functors {
 
   // binary tree
   trait Tree[+T]
@@ -9,10 +9,22 @@ class Functors {
 
   import cats.Functor
   implicit object TreeFunctor extends Functor[Tree] {
-    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = ???
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(v, l, r) => Branch(f(v), map(l)(f), map(r)(f))
+    }
   }
 
-  def main(args: Array[String]): Unit = {
+  def do10x[F[_]](container: F[Int])(implicit functor: Functor[F]): F[Int] =
+    functor.map(container)(_ * 10)
 
+  import cats.syntax.functor._
+  def do10xShorter[F[_]: Functor](container: F[Int]): F[Int] = container.map(_ * 10)
+
+
+  def main(args: Array[String]): Unit = {
+    val tree: Tree[Int] = Branch(1, Branch(2, Leaf(3), Leaf(4)), Leaf(5))
+    println(do10x(tree))
+    println(do10xShorter(tree))
   }
 }
